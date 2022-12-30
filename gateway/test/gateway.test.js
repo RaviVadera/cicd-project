2b81111d7279f64b0270e385d28598560fb5476f
Ravi Vadera <61351969+RaviVadera@users.noreply.github.com>
2022-12-30 16:14:59 +0000

    Added tests for /messages endpoint

--- gateway/test/gateway.test.js
+++ gateway/test/gateway.test.js
@@ -2,9 +2,20 @@ import { expect, test } from '@jest/globals';
 import request from 'supertest';
 import app from '../gateway';
 
-test('GATEWAY should expose /messages endpoint', async () => {
-    const res = await request(app)
-        .get('/messages')
-        .send();
-    expect(res.statusCode).toBe(200);
+describe('/messages', () => {
+    test('GATEWAY should expose /messages endpoint', async () => {
+        const res = await request(app)
+            .get('/messages')
+            .send();
+        expect(res.statusCode).toBe(200);
+    });
+
+    test('GATEWAY /messages endpoint should respond with text/plain', async () => {
+        const res = await request(app)
+            .get('/messages')
+            .send();
+        expect(res.statusCode).toBe(200);
+        expect(res.headers).toHaveProperty('content-type');
+        expect(res.headers['content-type']).toBe('text/plain; charset=utf-8');
+    });
 });
