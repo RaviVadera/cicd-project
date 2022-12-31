cb231b182aa8e726b174a32e0c6ba58674c839da
Ravi Vadera <61351969+RaviVadera@users.noreply.github.com>
2022-12-31 19:01:24 +0000

    Updated test for /messages content

--- gateway/test/gateway.test.js
+++ gateway/test/gateway.test.js
@@ -1,9 +1,29 @@
-import { jest, expect, test } from '@jest/globals';
+import { jest, expect, beforeAll, test } from '@jest/globals';
 import axios from 'axios';
 import request from 'supertest';
 import app from '../gateway';
 
+const expectedContent = `2022-11-23T14:58:40.345Z 1 MSG_1 to compse140.o\n
+  2022-11-23T14:58:41.348Z 2 Got MSG_1 to compse140.i\n
+  2022-11-23T14:58:43.349Z 3 MSG_2 to compse140.o\n
+  2022-11-23T14:58:44.352Z 4 Got MSG_2 to compse140.i\n
+  2022-11-23T14:58:46.353Z 5 MSG_3 to compse140.o\n
+  2022-11-23T14:58:47.356Z 6 Got MSG_3 to compse140.i`;
+
 describe('/messages', () => {
+  beforeAll(() => {
+    // since we are using axios to request data from HTTPSERV
+    // this may not be a good idea as changing implementation breaks tests
+    // or in worst case, provides a false image of everything good
+    const mockedResponse = {
+      status: 200,
+      headers: { 'Content-Type': 'text/plain; charset=utf-8' },
+      data: expectedContent,
+    };
+    axios.get = jest.fn();
+    axios.get.mockResolvedValue(mockedResponse);
+  });
+
   test('/messages endpoint should be available', async () => {
     const res = await request(app).get('/messages').send();
     expect(res.statusCode).toBe(200);
@@ -17,18 +37,6 @@ describe('/messages', () => {
   });
 
   test('/messages endpoint should respond the content from HTTPSERV', async () => {
-    const expectedContent = `2022-11-23T14:58:40.345Z 1 MSG_1 to compse140.o\n
-            2022-11-23T14:58:41.348Z 2 Got MSG_1 to compse140.i\n
-            2022-11-23T14:58:43.349Z 3 MSG_2 to compse140.o\n
-            2022-11-23T14:58:44.352Z 4 Got MSG_2 to compse140.i\n
-            2022-11-23T14:58:46.353Z 5 MSG_3 to compse140.o\n
-            2022-11-23T14:58:47.356Z 6 Got MSG_3 to compse140.i`;
-
-    // since we are using axios to request data from HTTPSERV
-    const mockedResponse = { status: 200, data: expectedContent };
-    axios.get = jest.fn();
-    axios.get.mockImplementation(() => Promise.resolve(mockedResponse));
-
     const res = await request(app).get('/messages').send();
     expect(res.statusCode).toBe(200);
     expect(res.headers).toHaveProperty('content-type');
