299526a8d0a473937ea5f39d785893290ad0a8c8
Ravi Vadera <61351969+RaviVadera@users.noreply.github.com>
2022-12-31 12:17:34 +0000

    Added dummy implementation for /messages endpoint

--- gateway/gateway.js
+++ gateway/gateway.js
@@ -2,9 +2,11 @@ import express from 'express';
 
 const app = express();
 
-// TODO return the messages entries from file
-// app.get('/messages', (req, res) => {
-// });
+// TODO return the messages entries from HTTPSERV
+app.get('/messages', (req, res) => {
+    res.header('Content-Type', 'text/plain; charset=utf-8');
+    res.send('Hello World!');
+});
 
 // TODO update the state
 // TODO return the current state
