1f0b9399510c917df126c481e41bf4b010a6b7b2
Ravi Vadera <61351969+RaviVadera@users.noreply.github.com>
2023-01-09 21:09:30 +0000

    Modified ORIG to send indefinitely

--- orig/index.js
+++ orig/index.js
@@ -1,80 +1,47 @@
-import amqp from 'amqplib';
+import express from "express";
+import cron from "node-cron";
+import amqp from "amqplib";
 
-const mqHost = 'mq';
-const mqExchange = 'topic_messages';
-const mqTopic = 'compse140.o';
-
+const mqHost = "mq";
+const mqExchange = "topic_messages";
+const mqTopic = "compse140.o";
 const mqConnection = await amqp.connect(`amqp://${mqHost}`);
 const mqChannel = await mqConnection.createChannel();
-await mqChannel.assertExchange(mqExchange, 'topic', { durable: false });
+await mqChannel.assertExchange(mqExchange, "topic", { durable: false });
 
-var messageNo = 1;
+const app = express();
+const port = 3000;
+
+let messageNo = 1;
 
 // create message content
 const getMessageContent = () => {
   return `MSG_${messageNo++}`;
 };
 
-// idle out for 1 day after messages are sent
-// I tried making the ORIG like an async task executor
-// but did not succeed, this should still be enough
-// as requirements did not specify any constraints
-// related to idling the ORIG
-const waitForExit = () => {
-  setTimeout(() => { }, 1 * 24 * 60 * 60 * 1000);
-};
+const scheduledTask = cron.schedule(
+  "*/3 * * * * *",
+  () => {
+    mqChannel.publish(mqExchange, mqTopic, Buffer.from(getMessageContent()));
+  },
+  { scheduled: false }
+);
 
-// wait for 3 seconds before sending message
-const sendMessageWithTimeout = (count) => {
-  setTimeout(() => {
-    if (count > 0) {
-      mqChannel.publish(mqExchange, mqTopic, Buffer.from(getMessageContent()));
-      sendMessageWithTimeout(count - 1);
-    }
-    else
-      waitForExit();
-  }, 3000);
-};
+// return the messages entries from file
+app.post("/start", (req, res) => {
+  scheduledTask.start();
+  res.header("Content-Type", "text/plain");
+  res.status(204).send().end();
+});
 
-// first message should be sent without timeout
-mqChannel.publish(mqExchange, mqTopic, Buffer.from(getMessageContent()));
-// consecutive 2 messages with timeout
-sendMessageWithTimeout(2);
+app.post("/stop", (req, res) => {
+  scheduledTask.stop();
+  res.header("Content-Type", "text/plain");
+  res.status(204).send().end();
+});
 
-// /**
-//  * @param {Task} task 
-//  */
-// const executeTask = (task) => {
-//     if (!task) {
-//         setTimeout(() => {
-//             waitForTask(undefined);
-//         }, 10 * 1000);
-//     } else {
-//         setTimeout(() => {
-//             mqChannel.publish(mqExchange, mqTopic, Buffer.from(task.messageToSend));
-//         }, task.waitMs);
-//     }
-// };
-
-// const waitForTask = () => {
-//     var running = true;
-
-//     function killProcess() {
-//         running = false;
-//     }
-
-//     process.on('SIGTERM', killProcess);
-//     process.on('SIGINT', killProcess);
-//     process.on('uncaughtException', function (e) {
-//         console.log('[uncaughtException] app will be terminated: ', e.stack);
-//         killProcess();
-//     });
-
-//     function run() {
-//         setTimeout(function () {
-//             if (running) run();
-//         }, 10);
-//     }
-
-//     run();
-// };
+// start listening for HTTP requests
+app.listen(port, () => {
+  scheduledTask.start();
+  console.log(`ORIG listening on port ${port}`);
+});
--- orig/state.js
+++
@@ -1,3 +0,0 @@
-var _state = 'not-sending';
-export function updateState(state) { _state = state; }
-export function getState() { return _state; }
