7b89f5f0f3d89ec51385bcd2df150db694c9b4c5
Ravi Vadera <61351969+RaviVadera@users.noreply.github.com>
2023-01-29 10:04:18 +0000

    Added implementation for GET /queue-statistic

--- gateway/gateway.js
+++ gateway/gateway.js
@@ -72,11 +72,13 @@ app.get('/node-statistic', async (req, res) => {
         password: 'guest',
       },
     });
-    // res.status(serviceResponse.status);
-    // Object.keys(serviceResponse.headers).forEach((serviceHeader) => {
-    //   res.header(serviceHeader, serviceResponse.headers[serviceHeader]);
-    // });
-    // return res.send(serviceResponse.data).end();
+    if (serviceResponse.status !== 200) {
+      res.status(serviceResponse.status);
+      Object.keys(serviceResponse.headers).forEach((serviceHeader) => {
+        res.header(serviceHeader, serviceResponse.headers[serviceHeader]);
+      });
+      return res.send(serviceResponse.data).end();
+    }
     // assuming the mq cluster has only one node
     // since no specific requirements were provided
     // on how to handle the metrics for multi-node cluster
@@ -97,5 +99,33 @@ app.get('/node-statistic', async (req, res) => {
 });
 
 // TODO return rabbitmq queue statistics
+app.get('/queue-statistic', async (req, res) => {
+  try {
+    const serviceResponse = await axios.get(`${mqManagementHost}/api/queues`, {
+      auth: {
+        username: 'guest',
+        password: 'guest',
+      },
+    });
+    if (serviceResponse.status !== 200) {
+      res.status(serviceResponse.status);
+      Object.keys(serviceResponse.headers).forEach((serviceHeader) => {
+        res.header(serviceHeader, serviceResponse.headers[serviceHeader]);
+      });
+      return res.send(serviceResponse.data).end();
+    }
+    const responseToSend = serviceResponse.data.map((queue) => ({
+      name: queue.name,
+      messages_delivered_recently: queue.message_stats.deliver_no_ack,
+      messages_delivery_Rate: queue.message_stats.deliver_no_ack_details.rate,
+      messages_published_lately: queue.message_stats.publish,
+      messages_publishing_rate: queue.message_stats.publish_details.rate,
+    }));
+    res.header('Content-Type', 'application/json; charset=utf-8');
+    return res.send(responseToSend).end();
+  } catch (error) {
+    return res.status(502).end();
+  }
+});
 
 export default app;
